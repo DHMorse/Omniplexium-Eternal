630dda136b93a16c3f2fd4f56d1e94fa5a6d9d85
404-5971 <141177946+404-5971@users.noreply.github.com>
2024-11-07 13:00:39 -0600

    updated main to use token from file

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import discord
 from discord.ext import commands
+from secret_const import TOKEN
 
 bot = commands.Bot(command_prefix='$', intents=discord.Intents.all()) #change the prefix to what ever i.e. "$" and some people call this var client instead of bot
 
@@ -8,4 +9,4 @@ async def on_ready():
     print(f'Bot is connected as {bot.user}')
 # not really super needed but nice
 
-bot.run("Your discord token which you get from discord dev portal or some shit")
+bot.run(TOKEN)
