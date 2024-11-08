a51671c907f0a2918f8ed50a1adc1ea8eec2bf65
Eli-Mason <elimasontech@icloud.com>
2024-11-07 21:24:43 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
+'''
+Two (three?) forms of currency:
+- Points: for sending messages and playing easy games (karma, aura, social status)
+- MONEY money: for leveling up, doing useful crap, etc. (money, stocks, economic crap)
+(- REAL MONEY MONEY money: buy with USD lol)
+
+EXTREME rewards for inviting people (automatic? manual?)
+'''
+
+
+
+
 import discord
 from discord.ext import commands
 from secret_const import TOKEN
 
-bot = commands.Bot(command_prefix='$', intents=discord.Intents.all()) #change the prefix to what ever i.e. "$" and some people call this var client instead of bot
+bot = commands.Bot(command_prefix='', intents=discord.Intents.all()) #change the prefix to what ever i.e. "$" and some people call this var client instead of bot
 
 @bot.event
 async def on_ready():
