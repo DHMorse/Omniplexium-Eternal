fad048806cce11a33ce256534764fd7925c6983c
Eli-Mason <155265813+Eli-Mason@users.noreply.github.com>
2024-11-08 20:58:05 -0500

    Update const.py with xpToLevel

--- const.py
+++ const.py
@@ -1,4 +1,5 @@
 import os
+import math
 
 ROOT_DIR = os.path.expanduser('~/Documents/Omniplexium-Eternal/')
 
@@ -7,3 +8,6 @@ CACHE_DIR_PFP = os.path.join(ROOT_DIR, 'cache_dir', 'pfps')
 LEADERBOARD_PIC = os.path.join(ROOT_DIR, 'leaderboard.png')
 
 DEFUALT_PROFILE_PIC = os.path.join(CACHE_DIR_PFP, 'defualt.png')
+
+def xpToLevel(experience, base=10):
+    return int(math.log(experience + 1, base)) + 1
