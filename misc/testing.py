a1a2f84823059db6c0a600e5bccc395d916638df
DHMorse <daniel@morsetech.net>
2024-11-30 20:37:09 -0600

    psuh

--- misc/testing.py
+++ misc/testing.py
@@ -11,3 +11,22 @@ mylist = [1, 2, 3, 4, 5]
 print(repr(str(mylist)))
 
 print(ast.literal_eval('[1, 2, 3, 4, 5]'))
+
+print(type(input("Enter a python list: ")))
+
+print("""\u001b[0;30mGray\u001b[0;0m
+\u001b[0;31mRed\u001b[0;0m
+\u001b[0;32mGreen\u001b[0;0m
+\u001b[0;33mYellow\u001b[0;0m
+\u001b[0;34mBlue\u001b[0;0m
+\u001b[0;35mPink\u001b[0;0m
+\u001b[0;36mCyan\u001b[0;0m
+\u001b[0;37mWhite\u001b[0;0m
+\u001b[0;40mFirefly dark blue background\u001b[0;0m
+\u001b[0;41mOrange background\u001b[0;0m
+\u001b[0;42mMarble blue background\u001b[0;0m
+\u001b[0;43mGreyish turquoise background\u001b[0;0m
+\u001b[0;44mGray background\u001b[0;0m
+\u001b[0;45mIndigo background\u001b[0;0m
+\u001b[0;46mLight gray background\u001b[0;0m
+\u001b[0;47mWhite background\u001b[0;0m""")
