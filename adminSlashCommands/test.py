cc10cccf9be1b0c4fd33989e219ae920fee5cafe
DHMorse <daniel@morsetech.net>
2025-02-02 14:16:09 -0600

    asdf

--- adminSlashCommands/test.py
+++ adminSlashCommands/test.py
@@ -5,6 +5,12 @@ from discord import app_commands
 async def admin_command(interaction: discord.Interaction):
     await interaction.response.send_message("You are an admin!", ephemeral=True)
 
+# Error handler for permission issues
+@admin_command.error
+async def admin_command_error(interaction: discord.Interaction, error):
+    if isinstance(error, app_commands.MissingPermissions):
+        await interaction.response.send_message("You don't have permission to use this command!", ephemeral=True)
+
 adminSlashCommandTest = app_commands.Command(
     name="admin_command", 
     description="This command is only for admins",
