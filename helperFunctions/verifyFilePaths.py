2e6a2897c860ee9ee90d6048bedbb15d8c0b88ee
DHMorse <daniel@morsetech.net>
2025-02-08 09:44:56 -0600

    maybe this fixes things

--- helperFunctions/verifyFilePaths.py
+++ helperFunctions/verifyFilePaths.py
@@ -5,13 +5,13 @@ from const import FILE_PATHS, COLORS
 
 async def verifyFilePaths(bot):
     for filePath in FILE_PATHS:
-        if not os.path.isdir(filePath) and not os.path.exists(filePath):
+        if not os.path.exists(filePath) and not filePath.endswith(('.png', '.jpg', '.jpeg', '.gif', '.webp', '.db')):
             os.makedirs(filePath)
             print(f"{COLORS['yellow']}Created {filePath}{COLORS['reset']}")
             await logWarning(bot, f"File path {filePath} did not exist, so it was created.")
         else:
             if not os.path.exists(filePath):
-                print(f"{COLORS['red']}Error: {filePath} does not exist{COLORS['reset']}")
-                await logWarning(bot, f"Error: {filePath} does not exist")
+                print(f"{COLORS['yellow']}Warning: {filePath} does not exist{COLORS['reset']}")
+                await logWarning(bot, f"{filePath} does not exist")
     
     print(f"{COLORS['blue']}Verified all file paths{COLORS['reset']}")
