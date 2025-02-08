a2f1c34ae200023ac9c50fd11652de108c8aeb9a
DHMorse <daniel@morsetech.net>
2025-02-08 09:52:14 -0600

    that makes sense

--- helperFunctions/verifyFilePaths.py
+++ helperFunctions/verifyFilePaths.py
@@ -5,14 +5,14 @@ from const import CACHE_PATH, FILE_PATHS, COLORS
 
 async def verifyFilePaths(bot):
     for filePath in FILE_PATHS:
-        if not os.path.exists(filePath) and not filePath.endswith(('.png', '.jpg', '.jpeg', '.gif', '.webp', '.db')):
-            if not CACHE_PATH in filePath:
+        if not CACHE_PATH in filePath:
+            if not os.path.exists(filePath) and not filePath.endswith(('.png', '.jpg', '.jpeg', '.gif', '.webp', '.db')):
                 os.makedirs(filePath)
                 print(f"{COLORS['yellow']}Created {filePath}{COLORS['reset']}")
                 await logWarning(bot, f"File path {filePath} did not exist, so it was created.")
-        else:
-            if not os.path.exists(filePath):
-                print(f"{COLORS['yellow']}Warning: {filePath} does not exist{COLORS['reset']}")
-                await logWarning(bot, f"{filePath} does not exist")
+            else:
+                if not os.path.exists(filePath):
+                    print(f"{COLORS['yellow']}Warning: {filePath} does not exist{COLORS['reset']}")
+                    await logWarning(bot, f"{filePath} does not exist")
     
     print(f"{COLORS['blue']}Verified all file paths{COLORS['reset']}")
---
+++ misc/paths.py
@@ -0,0 +1,7 @@
+import os
+
+CACHE_DIR = '/home/daniel/Documents/myCode/Omniplexium-Eternal/cache'
+
+LEADERBOARD_PIC = '/home/daniel/Documents/myCode/Omniplexium-Eternal/cache/leaderboard.png'
+
+print(CACHE_DIR in LEADERBOARD_PIC) 
