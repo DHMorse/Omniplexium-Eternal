e113e78b685f13112a9c19c9ddfd529d418fe971
DHMorse <daniel@morsetech.net>
2025-02-08 20:17:46 -0600

    added a new viewCard command, it will need to be updated but I want to see the demo of it first

--- adminCommands/viewCard.py
+++ adminCommands/viewCard.py
@@ -1,64 +1,45 @@
+from discord.ext import commands
 import discord
 import sqlite3
 
-from discord.ext import commands
-from pathlib import Path
-
 from const import COLORS, DATABASE_PATH
-
-# Ensure CARD_DATA_IMAGES_PATH is a Path object
-CARD_DATA_IMAGES_PATH = '' #Path(CARD_DATA_IMAGES_PATH)
 
 @commands.command()
 async def viewcard(ctx, *, query: str = "") -> None:
     if not ctx.author.guild_permissions.administrator:
-        await ctx.send(f'''```ansi
-{COLORS['yellow']}You do not have the required permissions to use this command.{COLORS['reset']}
-```''')
+        await ctx.send(f'''```ansi\n{COLORS['yellow']}You do not have the required permissions to use this command.{COLORS['reset']}\n```''')
+
+    if not query:
+        await ctx.send(f'''```ansi\n{COLORS['red']}Please specify a valid card name or ID to view.{COLORS['reset']}\n```''')
         return
-    
+
     with sqlite3.connect(DATABASE_PATH) as conn:
-        cursor = conn.cursor(dictionary=True)
+        cursor = conn.cursor()
+        
+        if query.isdigit():
+            cursor.execute("SELECT * FROM cards WHERE itemId = ?", (int(query),))
+            cardData = cursor.fetchone()
 
-        try:
-            if not query:
-                await ctx.send(f'''```ansi
-{COLORS['red']}Please specify a valid card name or ID to view.{COLORS['reset']}
-```''')
+            if not cardData:
+                await ctx.send(f'''```ansi\n{COLORS['red']}No card with that ID was found.{COLORS['reset']}\n```''')
                 return
-
-            cardData = None
-
-            # Try to find by itemId if the query is numeric
-            if query.isdigit():
-                cursor.execute("SELECT * FROM cards WHERE itemId = ?", (int(query),))
-                cardData = cursor.fetchone()
-            else:
-                # Otherwise, find by itemName (case-insensitive)
-                cursor.execute("SELECT * FROM cards WHERE LOWER(itemName) = LOWER(?)", (query,))
-                cardData = cursor.fetchone()
             
-            if cardData is None:
-                await ctx.send(f'''```ansi
-{COLORS['red']}No card found for '{query}'.{COLORS['reset']}
-```''')
+            cardPath = cardData[7]
+
+            file = discord.File(cardPath, filename="card.png")
+
+            await ctx.send(file=file)
+
+        else:
+            cursor.execute("SELECT * FROM cards WHERE itemName = ?", (query,))
+            cardData = cursor.fetchone()
+
+            if not cardData:
+                await ctx.send(f'''```ansi\n{COLORS['red']}No card with that name was found.{COLORS['reset']}\n```''')
                 return
+            
+            cardPath = cardData[7]
 
-            # Extract the itemId and construct the image path
-            item_id = cardData['itemId']
-            image_path = CARD_DATA_IMAGES_PATH / f"{item_id}.png"
+            file = discord.File(cardPath, filename="card.png")
 
-            # Check if the image file exists
-            if not image_path.exists():
-                await ctx.send(f'''```ansi
-{COLORS['red']}Image for card with ID {item_id} not found.{COLORS['reset']}
-```''')
-                return
-
-            # Send the image
-            await ctx.send(file=discord.File(image_path))
-        except Exception as e:
-            await ctx.send(f'''```ansi
-{COLORS['red']}An error occurred: {e}\u001b[0m{COLORS['reset']}
-```''')
-            return
+            await ctx.send(file=file)
