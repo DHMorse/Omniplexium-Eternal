d8c7d68700dad471d959cf4c2ec90b3275e0e55a
DHMorse <daniel@morsetech.net>
2025-02-09 21:25:50 -0600

    my code is fucking me as hard as depression does

--- slashCommands/startQuest.py
+++ slashCommands/startQuest.py
@@ -4,10 +4,10 @@ import asyncio
 from adventureStoryTime import *
 
 async def startQuestFunc(interaction: discord.Interaction) -> None:
-    interaction.response.defer()
+    await interaction.response.defer()
     hi = StoryMaker()
     var = await hi.startStory('You are a bear looking at the eclipse.', 169)
-    await interaction.response.send_message(var)
+    await interaction.followup.send(var)
 
 slashCommandStartQuest = app_commands.Command(
     name="start-quest", # no spaces or capitals allowed
