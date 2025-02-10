4d4d59cb40d99834b0260790c96269cbbfa20b8e
DHMorse <daniel@morsetech.net>
2025-02-09 21:20:59 -0600

    fdas

--- slashCommands/startQuest.py
+++ slashCommands/startQuest.py
@@ -5,7 +5,8 @@ from adventureStoryTime import *
 
 async def startQuestFunc(interaction: discord.Interaction) -> None:
     hi = StoryMaker()
-    await interaction.response.send_message(asyncio.run(hi.startStory('You are a bear looking at the eclipse.', 169)))
+    var = await hi.startStory('You are a bear looking at the eclipse.', 169)
+    await interaction.response.send_message(var)
 
 slashCommandStartQuest = app_commands.Command(
     name="start-quest", # no spaces or capitals allowed
